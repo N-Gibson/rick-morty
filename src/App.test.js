749c9b99dfdb412a5ead66d8fa24bb1eb0d3dd64
Noah Gibson <noah.gibson99@gmail.com>
2019-11-04 10:06:08 -0700

    Add testing for map dispatch in app

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { App } from './App';
 import { shallow } from 'enzyme';
-import { mapStateToProps } from './App'
+import { mapStateToProps, mapDispatchToProps } from './App';
+import { addEpisodes, addCharacters, addLocations, addCurrentDisplay } from './actions/index';
 
 describe('App', () => {
-  let wrapper;
+  let wrapper, mockDispatch, mappedDispatch;
 
   beforeEach(()=> {
-    wrapper = shallow(<App />)
+    wrapper = shallow(<App />);
+    mockDispatch = jest.fn();
+    mappedDispatch = mapDispatchToProps(mockDispatch)
   });
 
   it('mapStateToProps', () => {
@@ -28,5 +30,33 @@ describe('App', () => {
     const mappedProps = mapStateToProps(mockState)
 
     expect(mappedProps).toEqual(expected)
+  });
+
+  it('mapDispatchToProps addEpisodes', () => {
+    const actionToDispatch = addEpisodes([{episode: 'Rixlaxation'}]);
+    mappedDispatch.addEpisodes([{episode: 'Rixlaxation'}])
+
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  });
+
+  it('mapDispatchToProps addCharacters', () => {
+    const actionToDispatch = addCharacters([{name: 'Morty Sanchez'}]);
+    mappedDispatch.addCharacters([{name: 'Morty Sanchez'}]);
+
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  });
+
+  it('mapDispatchToProps addLocations', () => {
+    const actionToDispatch = addLocations([{name: 'Earth C-147'}]);
+    mappedDispatch.addLocations([{name: 'Earth C-147'}]);
+
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  });
+
+  it('mapDispatchToProps addCurrentDisplay', () => {
+    const actionToDispatch = addCurrentDisplay([{name: 'Earth C-147'}]);
+    mappedDispatch.addCurrentDisplay([{name: 'Earth C-147'}]);
+
+    expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
   })
 })
